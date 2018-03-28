ca36a736b2213133fc6e514d4929998a2c27db4e
Adrian Bindiu <adrian.bindiu@outlook.com>
2018-03-28 15:31:09 +0100

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  validate,
+  RomanToNumber,
+  checkForMultipleAppearence,
+  RomanNumberValues,
+  RomanNumberSpecialValues
+} from './utils';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('converter', () => {
+  it(`checks if the Roman Numerals are correct`, () => {
+    expect(RomanNumberValues).toEqual(
+      {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+      }
+    );
+  })
+
+  it(`checks if the Roman Special Numerals are correct`, () => {
+    expect(RomanNumberSpecialValues).toEqual(
+      {
+        "IV": 4,
+        "IX": 9,
+        "XL": 40,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900, 
+      }
+    );
+  })
+
+  it(`converts a simple roman numeral (VII) 
+  with no repetitions or special cases`, () => {
+    expect(RomanToNumber("VII")).toEqual(7);
+  })
+
+  it(`converts a more complicated roman numeral (XIV) 
+  with no repetitions but including special cases`, () => {
+    expect(RomanToNumber("XIV")).toEqual(14);
+  })
+
+  it(`converts a more complicated roman numeral (XIV) 
+  with repetitions and special cases`, () => {
+    expect(RomanToNumber("XIVIII")).toEqual(17);
+  })
+  
+  it(`checks if a normal roman numeral is validated correctly`, () => {
+    const number = RomanToNumber("XVIII");
+    expect(validate("XVIII", number)).toEqual(true);
+  })
+
+  it(`checks if a 4 times repetition is validated correctly`, () => {
+    expect(validate("VIIIIXII", null)).toEqual(false);
+  })
+
+  it(`checks if the function finds multiple appearences of a letter more than 3 times`, () => {
+    expect(checkForMultipleAppearence("VIIIIXII")).toEqual(true);
+  })
 });
--- src/utils.js
+++ src/utils.js
@@ -1,77 +1,77 @@
 export const RomanNumberValues = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-  }
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
   
 export const RomanNumberSpecialValues = {
-    "IV": 4,
-    "IX": 9,
-    "XL": 40,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900, 
-  }
+  "IV": 4,
+  "IX": 9,
+  "XL": 40,
+  "XC": 90,
+  "CD": 400,
+  "CM": 900, 
+}
   
 export const checkForMultipleAppearence = (input) => (/(.)\1\1\1/).test(input);
   
 export const validate = (input, value) => {
-    if (checkForMultipleAppearence(input)) return false;
-    if (value < 0) return false;
+  if (checkForMultipleAppearence(input)) return false;
+  if (value < 0) return false;
 
-    return true;
+  return true;
 }
   
 export const RomanToNumber = (input) => {
-    if (input.length < 2) return RomanNumberValues[input[0]];
-  
-    let number = 0;
-  
-    input.split('').map((letter, i) => {
-      const currentLetterValue = RomanNumberValues[letter];
-     
-      if (i === 0) { number = currentLetterValue; return; }
+  if (input.length < 2) return RomanNumberValues[input[0]];
+
+  let number = 0;
+
+  input.split('').map((letter, i) => {
+    const currentLetterValue = RomanNumberValues[letter];
     
-      const lastLetter = input[i - 1];
-      const lastLetterValue = RomanNumberValues[lastLetter];
-  
-      if (lastLetterValue < currentLetterValue) {
-        const constructedLetters = lastLetter + letter;
-        const constructedValue = RomanNumberSpecialValues[constructedLetters];
-        
-        // If it doesn't exist in the special values object
-        // then we should remove from our current value the total value from before
-        if (!constructedValue) {
-  
-          // If it's not in the special values object but the last letter
-          // is still smaller than the current one, then we should subtract the value
-          // of the last letter from the number and add up the difference between 
-          // the current letter value and last letter value ( Eg. VVX -> 10 (5 + (10-5)) )
-          if (lastLetterValue < currentLetterValue) {
-            number -= lastLetterValue;
-            number += currentLetterValue - lastLetterValue;
-  
-            return;
-          }
-  
-          number = currentLetterValue - number;
+    if (i === 0) { number = currentLetterValue; return; }
+
+    const lastLetter = input[i - 1];
+    const lastLetterValue = RomanNumberValues[lastLetter];
+
+    if (lastLetterValue < currentLetterValue) {
+      const constructedLetters = lastLetter + letter;
+      const constructedValue = RomanNumberSpecialValues[constructedLetters];
+      
+      // If it doesn't exist in the special values object
+      // then we should remove from our current value the total value from before
+      if (!constructedValue) {
+
+        // If it's not in the special values object but the last letter
+        // is still smaller than the current one, then we should subtract the value
+        // of the last letter from the number and add up the difference between 
+        // the current letter value and last letter value ( Eg. VVX -> 10 (5 + (10-5)) )
+        if (lastLetterValue < currentLetterValue) {
+          number -= lastLetterValue;
+          number += currentLetterValue - lastLetterValue;
+
           return;
         }
-  
-        number -= lastLetterValue;
-        number += RomanNumberSpecialValues[constructedLetters]
-        
+
+        number = currentLetterValue - number;
         return;
       }
-  
-      number += currentLetterValue;
-  
+
+      number -= lastLetterValue;
+      number += RomanNumberSpecialValues[constructedLetters]
+      
       return;
-    });
-  
-    return number;
+    }
+
+    number += currentLetterValue;
+
+    return;
+  });
+
+  return number;
 }
